d9911244094419f73a6568b01c2009da7e81e108
Ashish Goswami <147785669+AshishGoswami8055@users.noreply.github.com>
2023-10-13 08:25:45 +0530

    Add files via upload

---
+++ PATTERNS.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  //pattern 1
+  printf("          -\n");
+  printf("        /   \ \n");
+  printf("       /  |  \ \n");
+  printf("      / /   \ \ \n");
+  printf("     / /  |  \ \ \n");
+  printf("    / / / | \ \ \ \n");
+  printf("   / / /  |  \ \ \ \n");
+  printf("  / / / /   \ \ \ \ \n");
+  printf(" / / / /  |  \ \ \ \ \n");
+  printf("---------------------\n");
+  //pattern 2
+  printf("\n\t| | |\n");
+  printf("\t| | |\n");
+  printf("\t| | |\n");
+  printf("\t| | |\n");
+  printf("\t| | |\n");
+  printf("\t- - -\n");
+  getch();
+}
